a30a51c303b2620053b0fc01e38fab015b810a6f
lightjameslyy <lightjameslyy@gmail.com>
2017-12-01 17:34:32 +0800

    update Scheduler to CScheduler to coordinate with CThreadPool

---
+++ src/CScheduler.cpp
@@ -0,0 +1,54 @@
+#include "CScheduler.h"
+
+CThreadSafeQueue<CTask *> CScheduler::notReadyTaskQueue;
+
+CThreadSafeQueue<CTask *> CScheduler::readyTaskQueue;
+
+CScheduler *CScheduler::m_instance = nullptr;
+
+std::once_flag CScheduler::onceFlag;
+
+void CScheduler::init() { m_instance = new CScheduler(); }
+
+CScheduler *CScheduler::getInstance() {
+    std::call_once(onceFlag, init);
+    return m_instance;
+}
+
+void CScheduler::addTask(CTask* task) {
+    if (task->isDone()) {
+        return;
+    }
+    if (task->isReady()) {
+        readyTaskQueue.push(std::move(task));
+    } else {
+        notReadyTaskQueue.push(std::move(task));
+    }
+}
+
+std::shared_ptr<CTask*> CScheduler::getReadyTask() { return readyTaskQueue.try_pop(); }
+
+void CScheduler::schedule(CThreadPool& tpool) {
+    while (!notReadyTaskQueue.empty()) {
+        while (!readyTaskQueue.empty()) {
+            tpool.submit(*getReadyTask()); 
+        }
+        updateNotReadyTasks();
+    }
+    while (!readyTaskQueue.empty()) {
+        tpool.submit(*getReadyTask()); 
+    }
+}
+
+void CScheduler::updateNotReadyTasks() {
+    std::shared_ptr<CTask*> t = notReadyTaskQueue.try_pop();
+    if (!t)
+        return;
+    CTask* tp = *t;
+    if (tp->isDone())
+        notReadyTaskQueue.try_pop();
+    if (tp->isReady())
+        readyTaskQueue.push(std::move(tp));
+    else
+        notReadyTaskQueue.push(std::move(tp));
+}
---
+++ src/CScheduler.h
@@ -0,0 +1,32 @@
+#ifndef STAR_CSCHEDULER_H
+#define STAR_CSCHEDULER_H
+
+#include "CTask.h"
+#include "CThreadSafeQueue.h"
+#include "CThreadPool.h"
+#include <atomic>
+#include <mutex>
+#include <memory>
+#include <utility>
+
+class CScheduler {
+  private:
+    CScheduler() {}
+    CScheduler(const CScheduler &) {}
+    CScheduler &operator=(const CScheduler &) {}
+    ~CScheduler() {}
+    static CThreadSafeQueue<CTask *> notReadyTaskQueue;
+    static CThreadSafeQueue<CTask *> readyTaskQueue;
+    static CScheduler *m_instance;
+    static std::once_flag onceFlag;
+    static void init();
+    void updateNotReadyTasks();
+    std::shared_ptr<CTask*> getReadyTask();
+
+  public:
+    static CScheduler *getInstance();
+    void addTask(CTask*);
+    void schedule(CThreadPool&);
+};
+
+#endif // STAR_CSCHEDULER_H
